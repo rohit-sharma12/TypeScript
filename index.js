4726a1bada69f7906256643618410223a33506ef
rohitsharma <rs0043071@gmail.com>
2026-04-02 18:24:01 +0530

    intro to TS

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+var age = 200;
+var name1 = "Rohit";
+var college = "IGNOU";
+var male = true;
+var data = null;
+var test = undefined;
---
+++ index.ts
@@ -0,0 +1,8 @@
+var age: number = 200;
+
+var name1: string = "Rohit";
+var college: string = "IGNOU";
+
+var male: boolean = true;
+var data = null;
+var test = undefined;
